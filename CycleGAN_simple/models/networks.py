73daab206ae9edbc0f39f5d9a5a099730958366b
Patrick <patrick_youssef@hotmail.com>
2020-11-28 17:56:35 -0500

    more correction new generator

--- CycleGAN_simple/models/networks.py
+++ CycleGAN_simple/models/networks.py
@@ -164,7 +164,7 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
     elif netG == 'unet_256':
         net = UnetGenerator(input_nc, output_nc, 8, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'new':
-        net = NewGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6)
+        net = NewGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     else:
         raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
     return init_net(net, init_type, init_gain, gpu_ids)
