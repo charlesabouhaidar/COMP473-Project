44da49f8e0149b67b469ed9ce00bf4c75f961123
Charles Abou Haidar <charlesabouhaidar@protonmail.com>
2020-11-30 17:02:26 -0500

    added loss_to_graph function

--- CycleGAN_simple/util/util.py
+++ CycleGAN_simple/util/util.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 from PIL import Image
 import os
-
+import matplotlib.pyplot as plt
 
 def tensor2im(input_image, imtype=np.uint8):
     """"Converts a Tensor array into a numpy image array.
@@ -101,3 +101,91 @@ def mkdir(path):
     """
     if not os.path.exists(path):
         os.makedirs(path)
+
+def loss_to_graph(experiment_name, path_to_loss_file):
+    counter = 1
+    G_A_Loss = []
+    G_B_Loss = []
+    D_A_Loss = []
+    D_B_Loss = []
+    Cycle_A = []
+    Cycle_B = []
+    idt_A = []
+    idt_B = []
+
+    #line looks likes
+    #(epoch: 9, iters: 2891, time: 0.371, data: 0.001) D_A: 0.123 G_A: 0.724 cycle_A: 1.047 idt_A: 0.401 D_B: 0.145 G_B: 0.715 cycle_B: 0.866 idt_B: 0.478
+    with open(path_to_loss_file) as fp:
+        for i, line in enumerate(fp):
+            if i == 0 or line.find("Training Loss"):continue #skip first line of stats and any line that doesn't have any stats
+            position = line.find("G_A: ")
+            G_A_Loss.append(line[position+5 : position +10])
+
+            position = line.find("G_B: ")
+            G_B_Loss.append(line[position+5 : position +10])
+
+            position = line.find("D_A: ")
+            D_A_Loss.append(line[position+5 : position +10])
+
+            position = line.find("D_B: ")
+            D_B_Loss.append(line[position+5 : position +11])
+
+            position = line.find("cycle_A: ")
+            Cycle_A.append(line[position+9 : position +15])
+
+            position = line.find("cycle_B: ")
+            Cycle_B.append(line[position+9 : position +15])
+
+            position = line.find("idt_A: ")
+            idt_A.append(line[position+7 : position +13])
+
+            position = line.find("idt_B: ")
+            idt_B.append(line[position+7 : position +13])
+
+            counter = counter +1
+
+    #Generator losses
+    G_A = list(map(float, G_A_Loss))
+    G_B = list(map(float, G_B_Loss))
+    # Discriminator losses
+    D_A = list(map(float, D_A_Loss))
+    D_B = list(map(float, D_B_Loss))
+    # Cycle consistency losses
+    C_A = list(map(float, Cycle_A))
+    C_B = list(map(float, Cycle_B))
+    # Identity losses
+    ID_A = list(map(float, idt_A))
+    ID_B = list(map(float, idt_B))
+    experiment_name = "Relative Discriminator" ## LINE TO BE DELETED, WILL BE RECIEVIED AT THE TOP OF THE FUNCTION
+
+    #x axis
+    x = list(range(1, len(G_A_Loss)+1))
+
+    #y axis
+    fig = plt.figure(figsize=(16,20))
+    fig.suptitle(experiment_name)
+    ax1 = fig.add_subplot(311) ## generator plot
+    ax2 = fig.add_subplot(312) ## discriminator plot
+    ax3 = fig.add_subplot(313) ## Cycle consistency plot
+
+    #Graph 1
+    ax1.set_title('Generators')
+    ax1.set_xlabel('epochs')
+    ax1.set_ylabel('error loss')
+    ax1.plot(x,G_A, color="blue", label="Generator A")
+    ax1.plot(x,G_B, color="red", label="Generator B")
+    ax1.legend()
+    #Graph 2
+    ax2.set_title('Discriminators')
+    ax2.set_xlabel('epochs')
+    ax2.set_ylabel('error loss')
+    ax2.plot(x,D_A, color="blue", label="Discriminator A")
+    ax2.plot(x,D_B, color="red", label="Discriminator B")
+    ax2.legend()
+    #Graph 3
+    ax3.set_title('Cycle losses')
+    ax3.set_xlabel('epochs')
+    ax3.set_ylabel('error loss')
+    ax3.plot(x,C_A, color="blue", label="Cycle A")
+    ax3.plot(x,C_B, color="red", label="Cycle B")
+    ax3.legend()
